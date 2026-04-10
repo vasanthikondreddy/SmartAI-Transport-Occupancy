8c1f03ac80dec0d7f67e0a87158e871974e73add
vasanthikondreddy <k.vasanthi882005@gmail.com>
2026-04-10 22:15:06 +0530

    changed urls

--- frontend/src/pages/RouteComparePage.jsx
+++ frontend/src/pages/RouteComparePage.jsx
@@ -11,8 +11,8 @@ import {
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-const API_BASE = "http://localhost:8000"|| " import.meta.env.VITE_API_BASE_URL";
-
+// const API_BASE = "http://localhost:8000"|| " import.meta.env.VITE_API_BASE_URL";
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 const days   = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 const monthToSeason = {
